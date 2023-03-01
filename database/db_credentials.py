4f94a49724f0038ec11655c386d164022cf1b05f
Jakub Kowalski <81127430+JakubSero@users.noreply.github.com>
2023-03-01 11:41:05 -0600

    Add files via upload

---
+++ database/db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+user = 'cs340_kowaljak'       # don't forget the cs340_ prefix
+passwd = '2811'               # should only be 4 digits if default
+db = 'cs340_kowaljak'
