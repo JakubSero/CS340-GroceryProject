7f37945664d944e0e999d45d5fbb04b078538872
Jakub Kowalski <81127430+JakubSero@users.noreply.github.com>
2023-03-02 16:22:09 -0600

    Step 4 Changes

--- app.py
+++ app.py
@@ -20,19 +20,7 @@ db_connection = db.connect_to_database()
 # Routes
 @app.route('/')
 def root():
-	#query = "SELECT * FROM Customers;"
-	#query1 = "DROP TABLE IF EXISTS diagnostic;"
-	#query2 = "CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);"
-	#query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!");'
-	query4 = "SELECT * FROM Customers;"
-	cur = mysql.connection.cursor()
-	#cur.execute(query1)
-	#cur.execute(query2)
-	#cur.execute(query3)
-	cur.execute(query4)
-	results = cur.fetchall()
-	return "<h1> MYSQL Results" + str(results)
-	#return render_template("customers.html")	
+	return render_template("index.html")	
 
 
 @app.route('/customers')
@@ -50,7 +38,7 @@ def customers():
 # def customers():
 #     return render_template("receipts.html")
 
-@app.route('/items', methods=["POST", "GET", "UPDATE", "DELETE"])
+@app.route('/items', methods=["POST", "GET", "UPDATE"])
 def items():
     if request.method == "GET":
         query = "SELECT * FROM Items;"
@@ -58,7 +46,44 @@ def items():
         results = cursor.fetchall()
         return render_template("items.j2", Items=results)
 
+@app.route("/delete_item", methods=["POST", "GET", "DELETE"])
+def delete_item():
+    #pre_query = "SET foreign_key_checks = 0;"
+    query = "DELETE FROM Items WHERE item_num = '%s';" % request.form['item_num']
+    #after = "SET foreign_key_checks = 1;"
+    #check = db.execute_query(db_connection=db_connection, query=pre_query)
+    cur = db.execute_query(db_connection=db_connection, query=query)
+    #check2 = db.execute_query(db_connection=db_connection, query=after)
+    return redirect("/items")
 
+@app.route('/insert_item', methods=["POST"])
+def insert_item():
+    item_num = request.form["iNum"]
+    item_type = request.form["iType"]
+    retail_price = request.form["rPrice"]
+    quant_in_stock = request.form["qStock"]
+    query = f"INSERT INTO Items (item_num, item_type, retail_price, quant_in_stock) VALUES ({item_num}, '{item_type}', {retail_price}, {quant_in_stock});"
+
+    try:
+        db.execute_query(db_connection=db_connection, query=query)
+        return redirect("/items")
+    except Exception as e:
+        db_connection.rollback()
+        return "Error creating item: " + str(e)
+
+@app.route('/edit_item', methods=['POST'])
+def edit_people():
+    iNum = request.form['iNum']
+    iType = request.form['iType']
+    rPrice = request.form['rPrice']
+    qStock = request.form['qStock']
+    query = f"UPDATE Items SET item_type='{iType}', retail_price={rPrice}, quant_in_stock={qStock} WHERE item_num={iNum};"
+    try:
+        db.execute_query(db_connection=db_connection, query=query)
+        return redirect('/items')
+    except Exception as e:
+        db_connection.rollback()
+        return "Error updating item: " + str(e)
 
 # @app.route('/order_items')
 # def customers():
